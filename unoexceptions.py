3850c8f2c3ec2a6246bced0a48de431ddd7404f0
Erik Dymbe <erik.dymbe@gmail.com>
2022-10-04 10:40:22 +0200

    Remove unused exceptions

--- unoexceptions.py
+++ unoexceptions.py
@@ -2,21 +2,5 @@ class UnoException(Exception):
     pass
 
 
-class NotYourTurnException(UnoException):
-    pass
-
-
-class GameStartedException(UnoException):
-    pass
-
-
-class GameNotStartedException(UnoException):
-    pass
-
-
-class AliasTakenException(UnoException):
-    pass
-
-
 class IllegalMoveException(UnoException):
     pass
