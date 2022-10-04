4ddbeb6400b9533a008310470ddfa80cdf1ab090
Erik Dymbe <erik.dymbe@gmail.com>
2022-10-04 15:34:53 +0200

    Add uno-environment iterator

--- unoenv.py
+++ unoenv.py
@@ -13,11 +13,24 @@ class UnoEnvironment:
     def agent_iter(self):
         try:
             while not self.uno_game.game_won:
-                idx = self.uno_game.current_agent_idx
-                yield idx, self.agents[idx]
-            self.print_winner()
+                agent_idx = self.uno_game.current_agent_idx
+                yield agent_idx, self.agents[agent_idx]
         except StopIteration:
             pass
+
+    def __iter__(self):
+        yield self.uno_game.gamestate()
+        for agent_idx, agent in self.agent_iter():
+            observations = self.observations(agent_idx)
+            action = agent.get_action(observations, aliases=self.aliases)
+            try:
+                self.step(agent_idx, action)
+            except Exception as e:
+                print(e)
+                print("Invalid action:", action)
+                action = observations[-1].action_space().intersection([DrawCard(), SkipTurn(), AcceptDrawFour()]).pop()
+                self.step(agent_idx, action)
+            yield self.uno_game.gamestate()
 
     def step(self, agent_idx: int, action: Action):
         self.uno_game.step(agent_idx, action)
@@ -44,13 +57,11 @@ def main():
     from agents.textagent import TextAgent
 
     agents = [TextAgent("Player"), RandomAgent("RandomAgent 1"), RandomAgent("RandomAgent 2")]
-    aliases = [agent.alias for agent in agents]
     env = UnoEnvironment(agents=agents, winning_score=500)
-
-    for agent_idx, agent in env.agent_iter():
-        observations = env.observations(agent_idx)
-        action = agent.get_action(observations, aliases=aliases)
-        env.step(agent_idx, action)
+    for i, state in enumerate(env):
+        if state.game_won:
+            print(state.scores)
+            env.print_winner()
 
 
 if __name__ == '__main__':
