2ca52743a795c65b1b515fabf2c34cd292aecdaf
Erik Dymbe <erik.dymbe@gmail.com>
2022-10-06 16:17:48 +0200

    Add docstring to UnoAgent

--- agents/unoagent.py
+++ agents/unoagent.py
@@ -7,6 +7,19 @@ class UnoAgent(ABC):
     def __init__(self, alias):
         self.alias = alias
 
+    """
+    Gets the next action from the agent
+
+    Parameters
+    ----------
+    observations : list[Observation]
+        All observations of the game state so far for the current round (not the entire game). 
+    
+    Returns
+    -------
+    Action
+        The next action
+    """
     @abstractmethod
     def get_action(self, observations: list[Observation], **kwargs) -> Action:
         pass
